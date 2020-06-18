87d087086d797f2c7ff07342a353364598546212
Adao Oliveira <adaum79@gmail.com>
2020-06-18 20:38:14 -0300

    Add tela login

--- app/controllers/default.py
+++ app/controllers/default.py
@@ -1,8 +1,21 @@
 from flask import render_template
 from app import app
+from app.models.forms import LoginForm
 
 
-@app.route('/index/<user>')
-@app.route('/', defaults={'user': None})
-def index(user):
-    return render_template('base.html', user=user)
+@app.route('/index/')
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        print(form.username.data)
+        print(form.password.data)
+    else:
+        print(forms.errors)
+
+    return render_template('login.html', form=form)
